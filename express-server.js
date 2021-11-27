becbc877d0882bed67fdbff4ca83febfd02add74
Muhammad Khan <UmarP@Umars-MacBook-Pro.local>
2021-11-27 13:59:57 -0500

    Sending HTML

--- express-server.js
+++ express-server.js
@@ -15,6 +15,10 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
