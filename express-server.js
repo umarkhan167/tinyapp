079721fae121c0e2d19411abd156237193beeb08
Muhammad Khan <UmarP@Umars-MacBook-Pro.local>
2021-11-28 21:32:59 -0500

    surl shortening part 1 complete

--- express-server.js
+++ express-server.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+//STRING GENERATOR FUNCTION----------------------------------
+
+function generateRandomString() {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for ( var i = 0; i < 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * 
+    characters.length));
+  }
+  return result;
+}
+console.log(generateRandomString())
+
+//STRING GENERATOR FUNCTION----------------------------------
+
+
 
 app.set("view engine", "ejs");
 
@@ -9,13 +28,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
   };
 
+// app.post("/urls", (req, res) => {
+//   console.log(req.body);  // Log the POST request body to the console
+//   res.send("Ok");         // Respond with 'Ok' (we will replace this)
+// });
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase.shortURL};
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_shows", templateVars);
 });
 
